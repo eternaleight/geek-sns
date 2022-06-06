ef777d7649ea6a7183161a14aa24302d7e5bbb5e
eternaleight <eternaleight0@gmail.com>
2022-06-06 20:20:53 +0900

    feat(install): npm i -D postcss autoprefixer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import '../src/App.scss'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        aaa
       </header>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './src/pages/home/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  important: true,
+}
