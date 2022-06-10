6716e46da3cb7a0cc44f07f589dfedb379ad3529
eternaleight <eternaleight0@gmail.com>
2022-06-11 05:43:30 +0900

    fix(hover): fix hover motion

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -13,9 +13,10 @@ const style = {
   shareHr: `m-[20px]`,
   shareButtons: `flex justify-between`,
   shareOptions: `flex xs:ml-[10px] items-center`,
-  shareOptionText: `flex items-center mr-[15px] max-md:text-[14px] max-xs:text-[12px] cursor-pointer font-bold mediaButton`,
+  shareOptionText: `flex items-center mr-[15px] max-md:text-[14px] max-xs:text-[12px] cursor-pointer font-bold`,
   shareOptionText2: `delay-200 flex items-center mr-[15px] text-[12px] max-xs:text-[10px] cursor-pointer mediaButton2 absolute bg-[#33415599]  w-[28px] justify-center h-[20px] rounded-[2px] top-7 left-1 `,
-  shareIcon: `mr-[3px] max-xs:w-[20px]`,
+  mediaDiv: `absolute top-0 left-[-26px] w-[78px] cursor-pointer h-[24px] bg-[#00000000] mediaButton`,
+  shareIcon: `mr-[3px] max-xs:w-[20px] cursor-pointer`,
   shareButton: `mediaButton text-[14px] py-[6px] max-xs:px-[12px] px-[17px] bg-zinc-900 rounded-[5px]`,
 }
 
@@ -49,6 +50,7 @@ const Share = () => {
                 >
                   Media
                 </span>
+                <div className={style.mediaDiv}></div>
                 <span
                   className={`${style.shareOptionText2}`}
                 >
@@ -59,9 +61,12 @@ const Share = () => {
             <div className={style.shareOptions}>
               <Gif className={style.shareIcon} />
               <div className="relative flex flex-col">
-                <span className={`${style.shareOptionText}`}>
+                <span
+                  className={`${style.shareOptionText}`}
+                >
                   GIF
                 </span>
+                <div className={style.mediaDiv}></div>
                 <span
                   className={`${style.shareOptionText2} left-[-2px] h-[15px]`}
                 >
@@ -75,6 +80,7 @@ const Share = () => {
                 <span className={style.shareOptionText}>
                   Emoji
                 </span>
+                <div className={style.mediaDiv}></div>
                 <span
                   className={`${style.shareOptionText2} w-[40px] left-[-2px]`}
                 >
@@ -83,11 +89,12 @@ const Share = () => {
               </div>
             </div>
             <div className={style.shareOptions}>
-              <Analytics className={style.shareIcon} />
+              <Analytics className={`${style.shareIcon}`} />
               <div className="relative flex flex-col">
                 <span className={style.shareOptionText}>
                   Poll
                 </span>
+                <div className='absolute top-0 left-[-26px] w-[58px] cursor-pointer h-[24px] bg-[#00000000] mediaButton'></div>
                 <span
                   className={`${style.shareOptionText2} left-[-2px]`}
                 >
@@ -96,16 +103,16 @@ const Share = () => {
               </div>
             </div>
           </div>
-              <div className="relative flex flex-col">
-          <button className={style.shareButton}>
-            Post
-          </button>
-                <span
-                  className={`${style.shareOptionText2} top-[38px] left-[16px]`}
-                >
-                  投稿
-                </span>
-        </div>
+          <div className="relative flex flex-col">
+            <button className={style.shareButton}>
+              Post
+            </button>
+            <span
+              className={`${style.shareOptionText2} top-[38px] left-[16px]`}
+            >
+              投稿
+            </span>
+          </div>
         </div>
       </div>
     </div>
