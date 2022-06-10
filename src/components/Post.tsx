e239bb3a030ffeee6100d4ee28e192cf873c0582
eternaleight <eternaleight0@gmail.com>
2022-06-10 22:10:32 +0900

    feat(post): add post html

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,18 +1,41 @@
+import { MoreVert } from '@mui/icons-material'
+
 const Post = () => {
   return (
-    <div className="share">
-      <div className="shareWrapper">
-        <div className="shareTop">
+    <div className="post w-full max-xs:h-[150px] h-[170px] shadow-[3px_7px_13px_-10px_#65789f] rounded-[10px]">
+      <div className="postWrapper p-[10px]">
+        <div className="postTop">
           <img
-            src="/assets/person"
+            src="./assets/person/1.jpg"
             alt=""
-            className="shareProfileImg"
+            className="postProfileInput"
           />
-          <input
-            type="text"
-            className="shareInput"
-            placeholder="今何してるの？"
-          ></input>
+          <span className="postUserName">eternaleight</span>
+          <span className="postData">5分前</span>
+        </div>
+        <div className="postRight">
+          <MoreVert />
+        </div>
+        <div className="postCenter">
+          <span className="postText">SNS</span>
+          <img className="postImg" src="./assets/post/1.jpg" alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img
+              src="./assets/heart.png"
+              alt=""
+              className="likeIcon"
+            />
+            <span className="postLikeCounter">
+              5人がいいねを押しました
+            </span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postConmmentText">
+              4:コメント
+            </span>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,11 +1,12 @@
 import Share from './Share'
+import Post from './Post'
 
 const Timeline: React.FC = () => {
   return (
     <div className="timeline flex-[6]">
       <div className="timelineWrapper p-[20px]">
         <Share />
-        {/* <Post/> */}
+        <Post />
       </div>
     </div>
   )
