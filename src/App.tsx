244fe2d7d94f3473717d53a5284b03561ac53e99
eternaleight <eternaleight0@gmail.com>
2022-06-13 16:19:24 +0900

    feat(test): add 'change' test button & scrollTo top geek sns title logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.scss'
 import Home from './pages/Home'
+import Profile from './pages/Profile'
 
-const App:React.FC = () => {
+const App: React.FC = () => {
+  const [button, setButton] = useState<boolean>(false)
+  const Button = () => {
+    setButton(!button)
+  }
   return (
     <div className="App">
-      <Home />
+      <div onClick={() => Button()} className='sticky top-0 text-center cursor-pointer bg-zinc-800'>Change</div>
+      {button ? <Home /> : <Profile />}
     </div>
   )
 }
