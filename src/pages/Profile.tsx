c6440b7b1a80b6edeb16e36f2e235bc59412fb65
eternaleight <eternaleight0@gmail.com>
2022-06-15 02:00:01 +0900

    fix(ui): change login button color & setting Profile media query

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,49 +1,108 @@
+import { useState, useEffect } from 'react'
 import Rightbar from '../components/Rightbar'
 import Sidebar from '../components/Sidebar'
 import Topbar from '../components/Topbar'
 import TimeLine from '../components/Timeline'
 
 const Profile: React.FC = () => {
+  const [screenSize, setScreenSize] = useState<number>(
+    window.innerWidth < 768 ? 767 : 769
+  )
+
+  const windowWidth = () => {
+    setScreenSize(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', windowWidth)
+    return () =>
+    window.removeEventListener('resize', windowWidth)
+  }, [])
+
   return (
     <>
-      <Topbar />
-      <div className="flex profile">
-        <Sidebar />
-        <div className="profileRight flex-[10.5]">
-          <div className="profileRightTop">
-            <div className="profileCover h-[280px] relative">
-              <img
-                src="assets/person/2.jpeg"
-                alt=""
-                className="w-[100%] h-[210px] object-cover profileCoverImg"
-              />
-              <img
-                src="assets/person/1.jpeg"
-                alt=""
-                className="absolute top-[150px] left-0 right-0 object-cover w-[130px] m-auto rounded-full p-3 border-[#202023] profileUserImg border-[1px]"
-              />
-            </div>
-            <div className="flex flex-col items-center">
-              <h2 className="font-bold text-[50px] titileName">
-                Ryoya Itabashi
-              </h2>
-              <div className="profileInfo">
-                <h4 className="profileInfoName">
-                  eternaleight
-                </h4>
-                <span className="relative left-[-4px] profileInfoDesc">
-                  個人開発/nvim
-                </span>
+      {(screenSize < 768) ? (
+        <>
+          <Topbar />
+          <div className="flex profile">
+            <div className="profileRight flex-[10.5]">
+              <div className="profileRightTop">
+                <div className="profileCover h-[280px] relative">
+                  <img
+                    src="assets/person/2.jpeg"
+                    alt=""
+                    className="w-[100%] h-[210px] object-cover profileCoverImg"
+                  />
+                  <img
+                    src="assets/person/1.jpeg"
+                    alt=""
+                    className="absolute top-[150px] left-0 right-0 object-cover w-[130px] m-auto rounded-full p-3 border-[#202023] profileUserImg border-[1px]"
+                  />
+                </div>
+                <div className="flex flex-col items-center">
+                  <h2 className="font-bold text-[50px] titileName">
+                    Ryoya Itabashi
+                  </h2>
+                  <div className="profileInfo">
+                    <h4 className="profileInfoName">
+                      eternaleight
+                    </h4>
+                    <span className="relative left-[-4px] profileInfoDesc">
+                      個人開発/nvim
+                    </span>
+                  </div>
+                </div>
+                <div className="profilerightBottom"></div>
+              </div>
+              <div className="flex">
+                <TimeLine />
+                <Rightbar profile />
               </div>
             </div>
-            <div className="profilerightBottom"></div>
           </div>
-          <div className='flex'>
-          <TimeLine />
-          <Rightbar profile/>
+        </>
+      ) : 
+        <>
+          <Topbar />
+          <div className="flex profile">
+            <Sidebar />
+            <div className="profileRight flex-[10.5]">
+              <div className="profileRightTop">
+                <div className="profileCover h-[280px] relative">
+                  <img
+                    src="assets/person/2.jpeg"
+                    alt=""
+                    className="w-[100%] h-[210px] object-cover profileCoverImg"
+                  />
+                  <img
+                    src="assets/person/1.jpeg"
+                    alt=""
+                    className="absolute top-[150px] left-0 right-0 object-cover w-[130px] m-auto rounded-full p-3 border-[#202023] profileUserImg border-[1px]"
+                  />
+                </div>
+                <div className="flex flex-col items-center">
+                  <h2 className="font-bold text-[50px] titileName">
+                    Ryoya Itabashi
+                  </h2>
+                  <div className="profileInfo">
+                    <h4 className="profileInfoName">
+                      eternaleight
+                    </h4>
+                    <span className="relative left-[-4px] profileInfoDesc">
+                      個人開発/nvim
+                    </span>
+                  </div>
+                </div>
+                <div className="profilerightBottom"></div>
+              </div>
+              <div className="flex">
+                <TimeLine />
+                <Rightbar profile />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      }
     </>
   )
 }
