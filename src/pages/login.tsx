cff318a131f8c87594e914a639ae016d85f7b987
eternaleight <eternaleight0@gmail.com>
2022-06-15 00:16:51 +0900

    feat(login): add login page features

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,10 @@
 const Login = () => {
+  const style = {
+    loginInput: `focus:outline-none h-[50px] rounded-[10px] mb-1`,
+  }
   return (
     <>
-      <div className="login w-[100vw] h-[100vh] flex items-center justify-center">
+      <div className="login w-[100vw] h-[95.5vh] flex items-center justify-center">
         <div className="loginWrapper w-[70%] h-[70%] flex">
           <div className="loginleft flex-[1] flex flex-col justify-center">
             <h3 className="text-[50px] font-bold">
@@ -11,10 +14,36 @@ const Login = () => {
               hacker, programmer, 個人開発者のためのSNS
             </span>
           </div>
-          <div className="loginLeft flex-[1] ml-8 text-[22px] flex flex-col justify-center">
-            brilliant !! 💎
-            <br />
-            hacker community !! 💎
+          <div className="loginRight flex-[1] ml-8 text-[22px] flex flex-col justify-center">
+            <div className="loginRight">
+              <div className="loginBox h-[300px] p-[20px] flex flex-col justify-between bg-zinc-700 rounded-[10px] text-[16px]">
+                <p className="loginMsg text-[16px] relative top-[-8px]">
+                  ログインはこちらから
+                </p>
+                <input
+                  className={style.loginInput}
+                  type="text"
+                  placeholder="Email"
+                />
+                <input
+                  className={style.loginInput}
+                  type="text"
+                  placeholder="Password"
+                />
+                brilliant ✨
+                <br />
+                hacker community !! 💎
+                <button className="loginButton text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                  ログイン
+                </button>
+                  <span className="loginForget text-[15px]">
+                    パスワード忘れた方へ
+                  </span>
+                  <button className="loginRegisterButton text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                    アカウント作成
+                  </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
