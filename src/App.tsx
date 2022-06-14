03086f13422bd57c31c7cbaa57dfac11ff884e36
eternaleight <eternaleight0@gmail.com>
2022-06-15 06:09:38 +0900

    feat(resize): add size observer features

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import './App.scss'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
 import Login from './pages/login'
 
 const App: React.FC = () => {
-  const [screenSize, setScreenSize] = useState<number>(
-    window.innerWidth < 768 ? 767 : 769
-  )
-
-  const windowWidth = () => {
-    setScreenSize(window.innerWidth)
-  }
-
-  useEffect(() => {
-    window.addEventListener('resize', windowWidth)
-    return () =>
-      window.removeEventListener('resize', windowWidth)
-  }, [])
-
   const [button, setButton] = useState<boolean>(false)
   const Button = () => {
     setButton(!button)
   }
   return (
     <div className="App">
-      <Login screenSize={screenSize} />
+      <Login />
       <div
         onClick={() => Button()}
         className="sticky top-0 text-center cursor-pointer bg-zinc-800"
@@ -33,9 +19,9 @@ const App: React.FC = () => {
         Change
       </div>
       {button ? (
-        <Home screenSize={screenSize} button={button} />
+        <Home />
       ) : (
-        <Profile screenSize={screenSize} />
+        <Profile />
       )}
     </div>
   )
