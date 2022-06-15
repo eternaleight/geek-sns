858c58d1f2e860938b28923baeef961ace5c5de9
eternaleight <eternaleight0@gmail.com>
2022-06-15 19:43:45 +0900

    chore(import): change import position

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import SizeObserver from './utils/size-observer'
 import ScrollObserver from './utils/scroll-observer'
+import SizeObserver from './utils/size-observer'
 import LoginObserver from './utils/login-observer'
 
 const root = ReactDOM.createRoot(
