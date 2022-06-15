19554603232fdabf3ecccbd785f47e168b595a99
eternaleight <eternaleight0@gmail.com>
2022-06-16 00:18:37 +0900

    fix(style): features to change the color of the login button

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,4 @@
-import React, {
-  useContext,
-} from 'react'
+import React, { useContext } from 'react'
 import { ScrollContext } from '../utils/scroll-observer'
 import { SizeContext } from '../utils/size-observer'
 import { LoginContext } from '../utils/login-observer'
@@ -23,6 +21,7 @@ const Login: React.FC = () => {
   const style = {
     loginInput: `focus:outline-none h-[50px] w-full rounded-[4px] mb-1`,
     loginButton: `relative inline-flex items-center justify-center p-0.5 mb-2 mr-2  text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-2 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800`,
+    loginButton2: `text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2`,
   }
 
   return (
@@ -78,22 +77,41 @@ const Login: React.FC = () => {
                 brilliant ✨
                 <br />
                 hacker community !! 💎
-                <button
-                  onClick={() => login()}
-                  className={style.loginButton}
-                >
-                  <span className="relative w-[100%] py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                    ログイン
-                  </span>
-                </button>
-                <span className="loginForget text-[15px]">
-                  パスワード忘れた方へ
-                </span>
-                <button className={style.loginButton}>
-                  <span className="relative w-[100%] py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                    アカウント作成
-                  </span>
-                </button>
+                {innerWidth < 768 ? (
+                  <>
+                    <button
+                      onClick={() => login()}
+                      className={style.loginButton}
+                    >
+                      <span className="relative w-[100%] py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        ログイン
+                      </span>
+                    </button>
+                    <span className="loginForget text-[15px]">
+                      パスワード忘れた方へ
+                    </span>
+                    <button className={style.loginButton}>
+                      <span className="relative w-[100%] py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                        アカウント作成
+                      </span>
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <button
+                      onClick={() => login()}
+                      className={style.loginButton2}
+                    >
+                      ログイン
+                    </button>
+                    <span className="loginForget text-[15px]">
+                      パスワード忘れた方へ
+                    </span>
+                    <button className={style.loginButton2}>
+                      アカウント作成
+                    </button>
+                  </>
+                )}
               </div>
             </div>
           </div>
